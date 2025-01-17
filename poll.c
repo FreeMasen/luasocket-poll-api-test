cd8fe2ba4bca3a5ceaf7ce16161ae1b3bb846222
Robert Masen <robert.masen@smartthings.com>
2025-01-17 15:11:16 -0600

    fully working

--- poll.c
+++ poll.c
@@ -10,6 +10,18 @@
 
 static int global_poll(lua_State *L);
 
+static void dump_stack(lua_State *L)
+{
+    int i = lua_gettop(L);
+    // printf("vv STACK vv\n");
+    while (i > 0)
+    {
+        // printf("%i %s\n", i, lua_typename(L, lua_type(L, i)));
+        i -= 1;
+    }
+    // printf("^^^^^^^^^^^^\n");
+}
+
 /* functions in library namespace */
 static luaL_Reg func[] = {
     {"poll", global_poll},
@@ -25,7 +37,7 @@ int luaopen_poll(lua_State *L)
 static int actual_poll(lua_State *L, struct pollfd *fds, nfds_t nfds, int timeout)
 {
     int result = poll(fds, nfds, timeout * 1000);
-    printf("poll returned %i\n", result);
+    // printf("poll returned %i\n", result);
     if (result < 0)
     {
         char *error_msg;
@@ -72,52 +84,82 @@ static int getfd(lua_State *L)
     return fd;
 }
 
-static int collect_poll_args(lua_State *L, int tab, int itab, struct pollfd dest[])
+static int collect_poll_args(lua_State *L, int tab, int rtab, struct pollfd dest[])
 {
-    printf("collecting poll args %i -> %i\n", tab, itab);
+    // printf("collecting poll args %i -> %i\n", tab, rtab);
     if (lua_isnil(L, tab))
     {
-        printf("no args, returning early\n");
+        // printf("no args, returning early\n");
         return 0;
     }
     luaL_checktype(L, tab, LUA_TTABLE);
     int i = 1, n = 0, ret = 0;
     for (;;)
     {
-        printf("arg %i\n", i);
+        // printf("(%i) arg %i\n", lua_gettop(L), i);
         int fd;
         lua_pushinteger(L, i);
+        // printf("(%i) after lua_pushinteger %i\n", lua_gettop(L), i);
+        // dump_stack(L);
         lua_gettable(L, tab);
+        // printf("(%i) after lua_gettable %i\n", lua_gettop(L), i);
+        // dump_stack(L);
         if (lua_isnil(L, -1))
         {
-            printf("end of list %i\n", i);
+            // printf("end of list %i\n", i);
             lua_pop(L, 1);
+            // printf("(%i) after lua_pop %i\n", lua_gettop(L), i);
+            // dump_stack(L);
             break;
-        }
+        };
         int info = lua_gettop(L);
+        // printf("(%i) after lua_gettop %i %i\n", lua_gettop(L), i, info);
+        // dump_stack(L);
         lua_getfield(L, info, "sock");
+        // printf("(%i) after lua_getfield sock %i\n", lua_gettop(L), i);
+        // dump_stack(L);
         fd = getfd(L);
-        printf("%i fd: %i\n", i, fd);
-        lua_newtable(L);
+        // printf("(%i) after getfd %i -> %i\n", lua_gettop(L), i, fd);
+        // dump_stack(L);
         lua_pushnumber(L, (lua_Number)fd);
-        lua_settable(L, itab);
+        // printf("(%i) after lua_pushnumber %i\n", lua_gettop(L), i);
+        // swap fd and sock
+        lua_rotate(L, -2, 1);
+        dump_stack(L);
+        lua_settable(L, rtab);
+        // printf("(%i) after lua_settable %i\n", lua_gettop(L), i);
+        // dump_stack(L);
         if (fd != -1)
         {
             short events = POLLERR | POLLHUP;
             lua_getfield(L, info, "read");
-            if (lua_isboolean(L, 1))
+            // printf("(%i) after lua_getfield read %i\n", lua_gettop(L), i);
+            // dump_stack(L);
+            if (lua_toboolean(L, -1))
             {
-                printf("%i/%i is polling for read\n", i, fd);
+                // printf("%i/%i is polling for read\n", i, fd);
                 events |= POLLIN;
             }
+            // printf("(%i) after lua_toboolean %i\n", lua_gettop(L), i);
+            // dump_stack(L);
+            //  pop boolean property
             lua_pop(L, 1);
+            // printf("(%i) after lua_pop %i\n", lua_gettop(L), i);
+            // dump_stack(L);
             lua_getfield(L, info, "write");
-            if (lua_isboolean(L, 1))
+            // printf("(%i) after lua_getfield write %i\n", lua_gettop(L), i);
+            // dump_stack(L);
+            if (lua_toboolean(L, -1))
             {
-                printf("%i/%i is polling for write\n", i, fd);
+                // printf("%i/%i is polling for write\n", i, fd);
                 events |= POLLOUT;
             }
+            // printf("(%i) after lua_toboolean %i\n", lua_gettop(L), i);
+            // dump_stack(L);
+            //  pop boolean prop
             lua_pop(L, 1);
+            // printf("(%i) after lua_pop %i\n", lua_gettop(L), i);
+            // dump_stack(L);
             struct pollfd pfd = {
                 .fd = fd,
                 .events = events,
@@ -125,6 +167,7 @@ static int collect_poll_args(lua_State *L, int tab, int itab, struct pollfd dest
             };
             dest[n] = pfd;
         }
+        lua_pop(L, 1);
         n += 1;
         i += 1;
     }
@@ -136,14 +179,14 @@ static int global_poll(lua_State *L)
     int top, tab, to;
     struct pollfd fds[4096] = {0};
     top = lua_gettop(L);
-    printf("global_poll %i\n", top);
+    // printf("global_poll %i\n", top);
     if (top > 1)
     {
         to = luaL_optinteger(L, -1, 0);
-        printf("timeout %i\n", to);
+        // printf("timeout %i\n", to);
         lua_pop(L, 1);
         tab = lua_gettop(L);
-        printf("table %i\n", tab);
+        // printf("table %i\n", tab);
     }
     else
     {
@@ -151,13 +194,17 @@ static int global_poll(lua_State *L)
         tab = top;
     }
     lua_newtable(L);
-    int itab = lua_gettop(L);
-    int fd_ct = collect_poll_args(L, tab, itab, fds);
+    int rtab = lua_gettop(L);
+    // printf("before collect_poll_args %i\n", lua_gettop(L));
+    int fd_ct = collect_poll_args(L, tab, rtab, fds);
+    // printf("after collect_poll_args %i\n", lua_gettop(L));
     int result = actual_poll(L, fds, fd_ct, to);
     if (result < 0)
     {
         // error string was pushed onto the stack
         lua_pushnil(L);
+        // swap str,nil to nil, str
+        lua_rotate(L, -2, 1);
         return 2;
     }
     int ready_ct = 0;
@@ -167,13 +214,13 @@ static int global_poll(lua_State *L)
         struct pollfd *fd = &fds[i];
         if (fd == NULL)
         {
-            printf("fd was nil, exiting readiness check %i\n", i);
+            // printf("fd was nil, exiting readiness check %i\n", i);
             break;
         }
-        printf("fd %i may be ready %i/%i\n", fd->fd, fd->events, fd->revents);
-        if (fd->revents & POLLIN > 0 || fd->revents & POLLOUT > 0)
+        // printf("(%i) fd %i may be ready %i/%i\n", lua_gettop(L), fd->fd, fd->events, fd->revents);
+        if ((fd->revents & (POLLIN | POLLOUT)) > 0)
         {
-            printf("fd ready: %i\n", fd->fd);
+            // printf("(%i) fd ready: %i\n", lua_gettop(L), fd->fd);
             ready_ct += 1;
             if (ret_t == -1)
             {
@@ -182,21 +229,25 @@ static int global_poll(lua_State *L)
             }
             lua_newtable(L);
             int sock_t = lua_gettop(L);
-            lua_pushboolean(L, fd->revents & POLLIN > 0);
+            lua_pushboolean(L, fd->revents & POLLIN);
             lua_setfield(L, sock_t, "read");
-            lua_pushboolean(L, fd->revents & POLLOUT > 0);
+            lua_pushboolean(L, fd->revents & POLLOUT);
             lua_setfield(L, sock_t, "write");
             lua_pushinteger(L, fd->fd);
-            lua_gettable(L, itab);
+            lua_gettable(L, rtab);
+            // dump_stack(L);
             lua_setfield(L, sock_t, "sock");
+            lua_pushinteger(L, ready_ct);
+            lua_rotate(L, -2, 1);
             lua_settable(L, ret_t);
         }
     }
+    // dump_stack(L);
     if (ready_ct == 0)
     {
-        lua_pushstring(L, "timeout");
         lua_pushnil(L);
-        return -2;
+        lua_pushstring(L, "timeout");
+        return 2;
     }
     lua_pushvalue(L, ret_t);
     return 1;
--- test.lua
+++ test.lua
@@ -15,6 +15,7 @@ local ret, err = poll.poll({{
   read = true,
   write = false,
 }}, 1)
+print("first select", ret, err)
 -- nothing should be ready
 assert(ret == nil, string.format("expected error found %s", ret))
 assert(err == "timeout", string.format("expected 'timeout' found '%s'", err))
@@ -29,8 +30,34 @@ local writable = assert(poll.poll({{
   read = false,
   write = true,
 }}, 1))
+for i, info in ipairs(writable) do
+  print(string.rep("*", 5) .. tostring(i) .. string.rep("*", 5))
+  for k, v in pairs(info) do
+    print("  " .. k .. " = " .. tostring(v))
+  end
+  print(string.rep("*", 15))
+end
 assert(#writable == 2, "sockets not writable")
--- nothing should be ready
--- assert(ret == nil, string.format("expected error found %s", ret))
+assert(writable[1].sock == sock, "unexpected socket for writable[1]")
+assert(not writable[1].read, "sock1 was readable")
+assert(writable[1].write, "sock1 was not writable")
+assert(writable[2].sock == sock2, "unexpected socket for writable[2]")
+assert(not writable[2].read, "sock2 was readable")
+assert(writable[2].write, "sock2 was not writable")
 -- make sock ready to read
 sock2:sendto("some data", sock:getsockname())
+
+local readable = assert(poll.poll({{
+  sock = sock,
+  read = true,
+  write = false,
+}, {
+  sock = sock2,
+  read = true,
+  write = false,
+}}))
+
+assert(#readable == 1, "sockets not readable")
+assert(readable[1].sock == sock, "unexpected socket for writable[1]")
+assert(readable[1].read, "sock1 was not readable")
+assert(not readable[1].write, "sock1 was writable")
